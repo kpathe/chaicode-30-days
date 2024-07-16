96c178956002ef71da2530bed719884bbe49a3bb
Kaushlendra Pathe <pathekaushlendra@outlook.com>
2024-07-16 09:26:25 +0530

    Updated task.js and inclued print statements so that it tells us what the code is doing

--- 4 - Loops/task.js
+++ 4 - Loops/task.js
@@ -1,13 +1,17 @@
 // Activity 1: For Loop
 // 1.
+console.log(`Numbers from 1 to 10 using For Loop`);
 for (let i = 1; i <= 10; i++) {
     console.log(i)
 }
+console.log();
 
 // 2.
+console.log(`Multiplication Table of 5 using For Loop`);
 for (let i = 1; i <= 10; i++) {
     console.log(`5 x ${i} = ${5 * i}`)
 }
+console.log();
 
 // Activity 2: While Loop
 // 3.
@@ -17,19 +21,21 @@ while (i <= 10) {
     sum = sum + i
     i++
 }
-console.log(sum);
+console.log(`Sum of numbers from 1 to 10 : ${sum}`);
 console.log();
 
 // 4.
+console.log(`Numbers from 10 to 1 using While Loop`);
 let index = 10;
 while (index > 0) {
     console.log(index);
     index--;
 }
-console.log("")
+console.log()
 
 // Activity 3: Do...While Loop
 // 5.
+console.log(`Numbers from 1 to 5 using Do...While Loop`);
 i = 1
 do {
     console.log(i);
@@ -44,12 +50,13 @@ do {
     fact = fact * index;
     index++;
 } while (index <= num);
-console.log(fact); //is bar kar liya maine factorial. nhi to 1st sem ke c ke practical mei bna hi nhi tha 
+console.log(`Factorial of number ${num} : ${fact}`); //is bar kar liya maine factorial. nhi to 1st sem ke c ke practical mei bna hi nhi tha 
 // 08:44 16/07/2024
 console.log();
 
 // Activity 4: Nested Loops
 // 7.
+console.log(`Star Pattern using Nested For Loops`);
 for (let i = 1; i <= 5; i++) {
     for (let j = 1; j <= i; j++) {
         process.stdout.write("*"); //used this method after knowing it from the copilot to print the output in the same line
@@ -61,6 +68,7 @@ console.log();
 
 // Activity 5: Loop Control Statements
 // 8.
+console.log(`Use of continue to skip printing 5.`);
 for (let i = 1; i <= 10; i++) {
     if (i === 5) {
         continue;
@@ -70,6 +78,7 @@ for (let i = 1; i <= 10; i++) {
 console.log();
 
 // 9.
+console.log(`Use of break to stop loop once i becomes 7`);
 for (let i = 1; i <= 10; i++) {
     if (i === 7) {
         break;
