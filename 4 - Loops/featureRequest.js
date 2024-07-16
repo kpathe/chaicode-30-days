cfab9568dff3cab89160d30cba013ed7684f5ee2
Kaushlendra Pathe <pathekaushlendra@outlook.com>
2024-07-16 09:31:00 +0530

    Updated featureRequest.js in the same way as task.js

--- 4 - Loops/featureRequest.js
+++ 4 - Loops/featureRequest.js
@@ -1,11 +1,12 @@
 // 1. Number Printing Script
-// Using for loop
+console.log(`Number Printing Script`);
+console.log(`Using for loop`);
 for (let i = 1; i <= 10; i++) {
     console.log(i)
 }
 console.log();
 
-// Using while loop
+console.log(`Using while loop`);
 let i = 1;
 while (i <= 10) {
     console.log(i);
@@ -14,6 +15,8 @@ while (i <= 10) {
 console.log();
 
 // 2.Multiplication Table Script
+console.log(`Multiplication Table Script`);
+console.log(`Table of 5`);
 for (let i = 1; i <= 10; i++) {
     console.log(`5 x ${i} = ${5 * i}`)
 }
@@ -21,7 +24,8 @@ console.log();
 
 
 
-// 3.
+// 3. Pattern Printing Script
+console.log(`Pattern Printing Script`);
 for (let i = 1; i <= 5; i++) {
     for (let j = 1; j <= i; j++) {
         process.stdout.write("*"); //used this method after knowing it from the copilot to print the output in the same line
@@ -31,6 +35,7 @@ for (let i = 1; i <= 5; i++) {
 console.log();
 
 // 4.Sum Calculation Script
+console.log(`Sum Calculation Script`)
 i = 1;
 let sum = 0;
 while (i <= 10) {
@@ -41,6 +46,7 @@ console.log(sum);
 console.log();
 
 // 5. Factorial Calculation Script
+console.log(`Factorial Calculation Script`)
 let fact = 1
 index = 1
 let num = 6
